fa6fac0a35d4caeb04c57161ee1767284ee2551d
Pinxuan Huang <pinxhuang@gmail.com>
2019-05-24 20:54:00 +0400

    renamed home.html to index.html

--- quirky2/urls.py
+++ quirky2/urls.py
@@ -19,7 +19,7 @@ from django.views.generic.base import TemplateView
 from qr_code import urls as qr_code_urls
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
