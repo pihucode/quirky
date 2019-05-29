e34549b134ed86e6c9f329ad56bb8804497cd85d
Pinxuan Huang <pinxhuang@gmail.com>
2019-05-29 20:40:23 +0400

    added js functionality to nav

--- static/js/mainscript.js
+++ static/js/mainscript.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
-  //when menu icon is clicked, display a div containing nav links
+  // when menu icon is clicked, display a div containing nav links
   navMenu = document.getElementById('menu-icon');
   navMenu.addEventListener("click",function(){
       $("#nav-display").removeClass("hidden");
   });
 
-  //when closeButton is clicked, hide the div containing nav links
+  // when closeButton is clicked, hide the div containing nav links
   $("#close-button").click(function () {
       $("#nav-display").addClass("hidden");
   });
+
+  // TODO: print contents of div
+
 });
